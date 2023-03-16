6f28cb079feb682a93990beaa7494f1fee594311
fatdoge <704274491@qq.com>
2023-03-16 20:32:44 +0800

    feat: support markdown

--- src/components/MessageBox.tsx
+++ src/components/MessageBox.tsx
@@ -1,8 +1,45 @@
-import React, { FC } from 'react'
+import { FC } from 'react'
+import { Message } from '@interfaces'
+import MarkdownIt from 'markdown-it'
+import mdKatex from 'markdown-it-katex'
+import mdHighlight from 'markdown-it-highlightjs'
+import mdKbd from 'markdown-it-kbd'
+import { mockMarkdownData } from '@mock/markdown'
 
-const MessageBox: FC = () => {
+const defaultMessage: Message[] = [{
+  content: '\nhello!hello!hello!hello!hello!hello!\n\nhello!\n\nhello!hello!hello!hello!hello!hello!hello!\n\nhello!\n\nhello!',
+  role: 'assistant'
+}, {
+  content: 'hel\nlo!\n\n',
+  role: 'user'
+}, {
+  content: mockMarkdownData,
+  role: 'assistant'
+}]
+const MessageBox: FC<{ messages?: Message[] }> = ({ messages = defaultMessage }) => {
   return (
-    <>test</>
+    <div>
+      {messages.map(message => <MessageItem message={message}/>)}
+    </div>
+  )
+}
+
+const MessageItem: FC<{ message: Message }> = ({ message }) => {
+  const md = MarkdownIt({
+    linkify: true,
+    breaks: true
+  })
+    .use(mdKatex)
+    .use(mdHighlight, {
+      inline: true
+    })
+    .use(mdKbd)
+
+  return (
+    <div className={`flex mb-[8px] ${message.role === 'user' ? 'flex-row-reverse' : ''}`}>
+      <div dangerouslySetInnerHTML={{ __html: md.render(message.content) }} className={`p-4 ${message.role === 'user' ? 'bg-[#0086ff] text-white rounded-br-none' : 'rounded-bl-none bg-[#f1f2f6]'} break-words overflow-hidden rounded-[20px]`}>
+      </div>
+    </div>
   )
 }
 
---
+++ src/interfaces/index.ts
@@ -0,0 +1,4 @@
+export interface Message {
+  content: string
+  role: 'assistant' | 'user'
+}
