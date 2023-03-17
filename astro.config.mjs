5e5e60300d634440933fe0e5957c1e9ce7ca51c8
lvqq <nicksonlvqq@gmail.com>
2023-03-17 09:22:22 +0800

    fix: use edge instead of severless

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,6 +1,6 @@
 import { defineConfig } from 'astro/config';
 import solidJs from "@astrojs/solid-js";
-import vercel from "@astrojs/vercel/serverless";
+import vercel from "@astrojs/vercel/edge";
 import tailwind from "@astrojs/tailwind";
 
 import react from "@astrojs/react";
