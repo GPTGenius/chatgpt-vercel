44b7f24e7321f7445b46da6af65129f239b72573
lvqq <nicksonlvqq@gmail.com>
2023-04-09 15:45:13 +0800

    feat: support message time

--- src/components/MessageBox/index.tsx
+++ src/components/MessageBox/index.tsx
@@ -3,23 +3,25 @@ import { throttle } from 'lodash-es';
 import GlobalContext from '@contexts/global';
 import { ConversationMode, Message } from '@interfaces';
 import markdown from '@utils/markdown';
-import SystemAvatar from './Avatar/system';
+import { getRelativeTime } from '@utils/date';
+import SystemAvatar from '@components/Avatar/system';
+import './index.css';
 
-const MessageItem: FC<{ message: Message; key?: number }> = ({
+const MessageItem: FC<{ message: Message; index?: number }> = ({
   message,
-  key,
+  index,
 }) => {
   const { i18n } = useContext(GlobalContext);
   const isExpired = message.expiredAt && message.expiredAt <= Date.now();
-
+  const createdAt = getRelativeTime(message.createdAt, true);
   return (
     <div
-      className={`msg-fade-in flex items-start ${key === 0 ? '' : 'mt-[8px]'} ${
-        message.role === 'user' ? 'flex-row-reverse' : ''
-      }`}
+      className={`msg-fade-in flex items-start relative ${
+        index === 0 ? '' : 'mt-[24px]'
+      } ${message.role === 'user' ? 'flex-row-reverse' : ''}`}
     >
       {message.role === 'assistant' ? (
-        <SystemAvatar className="mt-[15px] mr-2" />
+        <SystemAvatar className="mt-[14px] mr-2" />
       ) : null}
       <div
         dangerouslySetInnerHTML={{
@@ -27,10 +29,19 @@ const MessageItem: FC<{ message: Message; key?: number }> = ({
             ? i18n.status_image_expired
             : markdown.render(message.content),
         }}
-        className={`prose shadow-sm p-4 ${
+        className={`prose message-box shadow-sm p-4 ${
           message.role === 'user' ? 'bg-gradient text-white' : 'bg-[#ebeced]'
         } break-words overflow-hidden rounded-[16px]`}
       />
+      {createdAt ? (
+        <div
+          className={`message-box-time invisible text-[#a1a7a8] text-sm absolute top-[-20px] ${
+            message.role === 'user' ? 'right-0' : 'left-[calc(32px+0.5rem)]'
+          }`}
+        >
+          {createdAt}
+        </div>
+      ) : null}
     </div>
   );
 };
@@ -83,7 +94,7 @@ const MessageBox: FC<{
         />
       ) : null}
       {messages.map((message, index) => (
-        <MessageItem key={index} message={message} />
+        <MessageItem key={index} index={index} message={message} />
       ))}
       {streamMessage ? (
         <MessageItem message={{ role: 'assistant', content: streamMessage }} />
--- src/utils/date.ts
+++ src/utils/date.ts
@@ -1,9 +1,9 @@
 import dayjs from 'dayjs';
 
-export const getRelativeTime = (time: number) => {
+export const getRelativeTime = (time: number, exact = false) => {
   const dayjsTime = dayjs(time);
   if (dayjsTime.isSame(dayjs(), 'date')) {
     return dayjsTime.format('HH:mm');
   }
-  return dayjsTime.format('MM-DD');
+  return dayjsTime.format(exact ? 'MM-DD' : 'MM-DD HH:mm');
 };
